6869e10bd732965b2b24a646cb56752994875c89
Felipe Santos <felipesantossf@outlook.com>
2024-03-08 21:08:19 -0300

    Initial Commit - criando uma API que consuma os dados da API do IBGE

---
+++ Controllers/IBGEApiNameController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("[controller]")]
+public class IBGENamesController : Controller
+{
+    [HttpGet(Name = "IBGE")]
+    public async Task<IActionResult> NumbersOfNames(string name)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            string apiUrl = $"https://servicodados.ibge.gov.br/api/v2/censos/nomes/{name}";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+
+                    return Content(result, "application/json");
+                }
+                else 
+                {
+                    return BadRequest($"Error! {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error! {ex.Message}");
+            }
+        }
+    }
+}
