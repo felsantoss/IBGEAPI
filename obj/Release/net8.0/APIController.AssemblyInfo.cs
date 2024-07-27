086bf82c4d2e461db5d0fbbc9ddf4ac5d5b166b5
Felipe Santos <felipesantossf@outlook.com>
2024-07-26 21:48:31 -0300

    commit

--- obj/Release/net8.0/APIController.AssemblyInfo.cs
+++ obj/Release/net8.0/APIController.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIController")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3f539a9a25b2dfbcdfe1c84896005c54946622")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIController")]
